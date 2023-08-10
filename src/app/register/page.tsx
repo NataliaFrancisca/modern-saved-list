84e6a4c6136b077d52acce7a4115306ed7f94449
Natalia Francisca <nathifrancisca@gmail.com>
2023-08-10 20:48:18 -0300

    feat: adding the validation on page register

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,35 +1,69 @@
+'use client';
 import Link from 'next/link';
 import AppTitle from '../components/app-title';
 import ButtonDefault from '../components/button-default';
+import { useState } from 'react';
+import { useRegister } from '../hooks/useRegister';
 
 const Register = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const { error, submitWithEmailPassword, submitWithGoogle } = useRegister({
+    name,
+    email,
+    password
+  });
+
   return (
     <section className="base-page">
       <AppTitle />
 
-      <form className="form-element">
+      <form className="form-element" onSubmit={() => submitWithEmailPassword()}>
         <legend className="page-legend">REGISTER.</legend>
 
         <div className="group-label-input">
           <label htmlFor="name-input">NAME:</label>
-          <input type="text" id="name-input" />
+          <input
+            type="text"
+            id="name-input"
+            onChange={(e) => setName(e.target.value)}
+          />
+          {error && <span>{error.name}</span>}
         </div>
 
         <div className="group-label-input">
           <label htmlFor="email-input">E-MAIL:</label>
-          <input type="email" id="email-input" />
+          <input
+            type="email"
+            id="email-input"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          {error && <span>{error.email}</span>}
         </div>
 
         <div className="group-label-input">
           <label htmlFor="password-input">PASSWORD:</label>
-          <input type="password" id="password-input" />
+          <input
+            type="password"
+            id="password-input"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          {error && <span>{error.password}</span>}
         </div>
 
         <ButtonDefault btnName="REGISTER" />
 
         <span className="txt-or">OR</span>
 
-        <button className="button-google">REGISTER WITH GOOGLE</button>
+        <button
+          className="button-google"
+          onClick={() => submitWithGoogle()}
+          type="button"
+        >
+          REGISTER WITH GOOGLE
+        </button>
       </form>
 
       <span className="nav-message-bottom">
