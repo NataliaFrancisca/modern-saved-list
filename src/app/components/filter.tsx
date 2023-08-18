dad7fcf69aad07b646677e65d0dcca431f52adff
Natalia Francisca <nathifrancisca@gmail.com>
2023-08-17 21:24:44 -0300

    fix: add link property

--- src/app/components/filter.tsx
+++ src/app/components/filter.tsx
@@ -21,32 +21,13 @@ const Filter = () => {
           <img src="icon/add.svg" alt="add icon" />
         </button>
 
-        <Content data={{ title: 'Pachinko', type: 'BOOK' }} />
-        <Content data={{ title: 'BK AO VIVO', type: 'VIDEO' }} />
-        <Content data={{ title: 'MANO A MANO', type: 'PODCAST' }} />
         <Content
           data={{
-            title: 'What Is Candomblé? Beliefs and History',
-            type: 'ARTICLE'
+            title: 'Pachinko',
+            link: 'https://www.amazon.com.br/Pachinko-Min-Jin-Lee/dp/8551006347',
+            type: 'BOOK'
           }}
         />
-        <Content data={{ title: 'RECEITA COXINHA FÁCIL', type: 'OTHERS' }} />
-        <Content
-          data={{ title: 'tudo sobre o amor - bell hooks', type: 'BOOK' }}
-        />
-        <Content data={{ title: 'amendoas', type: 'BOOK' }} />
-        <Content data={{ title: 'BK AO VIVO', type: 'VIDEO' }} />
-        <Content data={{ title: 'MANO A MANO', type: 'PODCAST' }} />
-        <Content
-          data={{
-            title: 'What Is Candomblé? Beliefs and History',
-            type: 'ARTICLE'
-          }}
-        />
-        <Content data={{ title: 'RECEITA COXINHA FÁCIL', type: 'OTHERS' }} />
-        <Content data={{ title: 'RECEITA COXINHA FÁCIL', type: 'OTHERS' }} />
-        <Content data={{ title: 'RECEITA COXINHA FÁCIL', type: 'OTHERS' }} />
-        <Content data={{ title: 'BK AO VIVO', type: 'VIDEO' }} />
       </section>
     </main>
   );
