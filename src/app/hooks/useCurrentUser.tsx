a552bb827d8d2da97cda43d48a0756cc803f10c1
Natalia Francisca <nathifrancisca@gmail.com>
2023-08-31 22:25:20 -0300

    feat: comment function

--- src/app/hooks/useCurrentUser.tsx
+++ src/app/hooks/useCurrentUser.tsx
@@ -1,26 +1,28 @@
-import { onAuthStateChanged } from 'firebase/auth';
-import { useState, useEffect } from 'react';
-import { auth } from '../firebase/config';
+// import { onAuthStateChanged } from 'firebase/auth';
+// import { useState, useEffect } from 'react';
+// import { auth } from '../firebase/config';
+// import { saveUserSession } from '../utils/local-storage/save-user';
 
-type User = {
-  displayName: string | null;
-  email: string | null;
-  uid: string;
-};
+// type User = {
+//   displayName: string | null;
+//   email: string | null;
+//   uid: string;
+// };
 
-export const useCurrentUser = () => {
-  const [user, setUser] = useState<User>();
+// export const useCurrentUser = () => {
+//   const [user, setUser] = useState<User>();
 
-  useEffect(() => {
-    onAuthStateChanged(auth, (currentUser) => {
-      if (currentUser) {
-        const { displayName, email, uid } = currentUser;
-        setUser({ displayName, email, uid });
-      } else {
-        return 'user not connect';
-      }
-    });
-  }, []);
+//   useEffect(() => {
+//     onAuthStateChanged(auth, (currentUser) => {
+//       if (currentUser) {
+//         const { displayName, email, uid } = currentUser;
+//         saveUserSession({ displayName, email, uid });
+//         // setUser({ displayName, email, uid });
+//       } else {
+//         return 'user not connect';
+//       }
+//     });
+//   }, []);
 
-  return user;
-};
+//   return user;
+// };
