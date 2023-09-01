8258814fb4bff4b7c324c30ec9da2d606961cf02
Natalia Francisca <nathifrancisca@gmail.com>
2023-08-31 22:23:58 -0300

    feat: change the filter grid to show not found data

--- src/app/components/filter.tsx
+++ src/app/components/filter.tsx
@@ -5,21 +5,23 @@ import { useGetContent } from '../hooks/useGetContent';
 import Content from './content';
 import { useRouter } from 'next/navigation';
 import React from 'react';
+import { useSearchParams } from 'next/navigation';
 
-const Filter = React.memo(() => {
-  const [loading, setLoading] = useState(true);
-  const userContent = useGetContent();
+const Filter = React.memo((props: { searchData: string }) => {
   const router = useRouter();
+  const routerSearch = useSearchParams();
+
+  const [loading, setLoading] = useState(true);
+  let filter = routerSearch.get('filter') || 'allcontent';
+
+  const userContent = useGetContent(
+    props.searchData.toUpperCase(),
+    filter.toUpperCase()
+  );
 
   useEffect(() => {
     userContent ? setLoading(false) : setLoading(true);
-  }, []);
-  // const [loading, setLoading] = useState(true);
-  // const routerSearch = useSearchParams();
-  // let currentFilter = routerSearch.get('filter') || 'allcontent';
-
-  // const router = useRouter();
-  // const data = useGetContent(currentFilter);
+  }, [userContent]);
 
   return (
     <main className="filter">
@@ -28,18 +30,41 @@ const Filter = React.memo(() => {
       ) : (
         <>
           <DashboardNav />
-          <section className="filter-result">
-            <button
-              className="btn-add-content"
-              onClick={() => router.push('/dashboard/save-content')}
-            >
-              <img src="icon/add.svg" alt="add icon" />
-            </button>
 
-            {userContent.map((content, index) => (
-              <Content data={content} key={index} />
-            ))}
-          </section>
+          {userContent && userContent.length == 0 && filter != 'allcontent' ? (
+            <section className="filter-result_nothing">
+              {/* <button
+                className="btn-add-content"
+                onClick={() => router.push('/dashboard/save-content')}
+              >
+                <img src="icon/add.svg" alt="add icon" />
+              </button> */}
+
+              <div className="image-illustration">
+                <img
+                  src="illustration/nothing-here.svg"
+                  alt="toy nothing here"
+                />
+                <h1>NOTHING HERE</h1>
+              </div>
+            </section>
+          ) : (
+            <section className="filter-result">
+              <>
+                <button
+                  className="btn-add-content"
+                  onClick={() => router.push('/dashboard/save-content')}
+                >
+                  <img src="icon/add.svg" alt="add icon" />
+                </button>
+
+                {userContent &&
+                  userContent.map((content, index) => (
+                    <Content data={content} key={index} />
+                  ))}
+              </>
+            </section>
+          )}
         </>
       )}
     </main>
