8f31e86fb1be596f35541fa0c303dc9cbb0390df
Natalia Francisca <nathifrancisca@gmail.com>
2023-08-31 22:24:12 -0300

    feat: add function to send the text typed

--- src/app/components/input-search.tsx
+++ src/app/components/input-search.tsx
@@ -1,17 +1,15 @@
 import { useState } from 'react';
 
-const InputSearch = (props: { toggleInput: boolean }) => {
+const InputSearch = (props: { onUpdateSearchData: Function }) => {
   const [searchText, setSearchText] = useState('');
 
   const onHandleSubmit = () => {
     event?.preventDefault();
+    props.onUpdateSearchData(searchText);
   };
 
   return (
-    <form
-      className={`form-search-content isHidden-${props.toggleInput}`}
-      onSubmit={() => onHandleSubmit()}
-    >
+    <form className={`form-search-content`} onSubmit={() => onHandleSubmit()}>
       <img src="icon/search.svg" alt="search icon" />
       <input
         type="text"
