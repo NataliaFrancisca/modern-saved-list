bfcb18f06ee512d9c058b0803eb305a644ba5dad
Natalia Francisca <nathifrancisca@gmail.com>
2023-08-31 22:24:33 -0300

    feat: get the user data from the local storage; remove the loading

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,54 +1,48 @@
 'use client';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import AppTitle from '../components/app-title';
-import Loader from '../components/loader';
-import { useCurrentUser } from '../hooks/useCurrentUser';
 import Filter from '../components/filter';
 import InputSearch from '../components/input-search';
+import { getUserSession } from '../utils/local-storage/save-user';
+import PrivateRoute from './private-page';
 
 const Dashboard = () => {
-  const [toggleInput, setToggleInput] = useState(true);
-  const [loading, setLoading] = useState(true);
+  const userInformation = getUserSession();
 
-  const userData = useCurrentUser();
+  const [toggleInput, setToggleInput] = useState(true);
+  const [searchData, setSearchData] = useState('');
 
   const onToggleInputSearch = () => setToggleInput(!toggleInput);
 
-  useEffect(() => {
-    userData ? setLoading(false) : setLoading(true);
-    () => {};
-  }, [userData]);
-
   return (
-    <main className="base-page">
-      <AppTitle />
-      <header className="header-dashboard">
-        <button>
-          <img
-            src="icon/search.svg"
-            alt="search icon"
-            onClick={() => onToggleInputSearch()}
-          />
-        </button>
+    <PrivateRoute>
+      <main className="base-page">
+        <AppTitle />
+        <header className="header-dashboard">
+          <button>
+            <img
+              src="icon/search.svg"
+              alt="search icon"
+              onClick={() => onToggleInputSearch()}
+            />
+          </button>
 
-        <img src="user-image/user-option-one.svg" alt="user illustration" />
+          <img src="user-image/user-option-one.svg" alt="user illustration" />
 
-        <button>
-          <img src="icon/menu.svg" alt="menu icon" />
-        </button>
-      </header>
-      {loading ? (
-        <Loader />
-      ) : (
+          <button>
+            <img src="icon/menu.svg" alt="menu icon" />
+          </button>
+        </header>
+
         <h1 className={`dashboard-user-name inputHidden-${toggleInput}`}>
-          OLÁ, {userData?.displayName}!
+          OLÁ, {userInformation?.displayName}!
         </h1>
-      )}
 
-      <InputSearch toggleInput={toggleInput} />
+        {!toggleInput && <InputSearch onUpdateSearchData={setSearchData} />}
 
-      <Filter />
-    </main>
+        <Filter searchData={searchData} />
+      </main>
+    </PrivateRoute>
   );
 };
 
