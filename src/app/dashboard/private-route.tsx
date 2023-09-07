e2881feecd306334276a31f97a53cea009c39305
Natalia Francisca <nathifrancisca@gmail.com>
2023-09-06 22:49:36 -0300

    feat: use loader to show only the page if the data is ok

--- src/app/dashboard/private-route.tsx
+++ src/app/dashboard/private-route.tsx
@@ -1,6 +1,7 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation'; // Importe seu contexto de autenticação
 import { getUserCookie } from '../utils/local-storage/save-user';
+import Loader from '../components/loader';
 
 interface PrivateRouteProps {
   children: React.ReactNode;
@@ -9,14 +10,24 @@ interface PrivateRouteProps {
 function PrivateRoute({ children }: PrivateRouteProps) {
   const router = useRouter();
   const userInformation = getUserCookie();
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     if (!userInformation) {
       router.push('/');
       return;
     }
-  }, [userInformation, router]);
 
-  return userInformation && <>{children}</>;
+    if (userInformation) {
+      setLoading(!loading);
+    }
+  }, []);
+
+  return loading ? (
+    <Loader color={'GREEN'} />
+  ) : (
+    <section className="base-page">{children}</section>
+  );
 }
 
 export default PrivateRoute;
