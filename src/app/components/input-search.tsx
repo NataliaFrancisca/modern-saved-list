8952722d46e533196f2220e3e8c96acf269519a6
Natalia Francisca <nathifrancisca@gmail.com>
2023-09-11 23:00:26 -0300

    feat: change the functions name; and props

--- src/app/components/input-search.tsx
+++ src/app/components/input-search.tsx
@@ -3,24 +3,27 @@ import { useRouter } from 'next/navigation';
 
 const InputSearch = (props: {
   onUpdateSearchData: Function;
-  closeInput: Function;
+  onToggleInput: Function;
 }) => {
   const [searchText, setSearchText] = useState('');
   const router = useRouter();
 
   const onHandleSubmit = () => {
     event?.preventDefault();
-    props.onUpdateSearchData(searchText);
+    props.onUpdateSearchData(searchText.toUpperCase());
   };
 
-  const onCancelSearch = () => {
+  const onCloseInput = () => {
     props.onUpdateSearchData('');
-    props.closeInput(true);
+    props.onToggleInput();
     router.push('/dashboard');
   };
 
   return (
-    <form className={`form-search-content`} onSubmit={() => onHandleSubmit()}>
+    <form
+      className="__dashboard-input-search"
+      onSubmit={() => onHandleSubmit()}
+    >
       <img src="icon/search.svg" alt="search icon" />
       <input
         type="text"
@@ -30,7 +33,7 @@ const InputSearch = (props: {
       <img
         src="icon/cancel.svg"
         alt="cancel icon"
-        onClick={() => onCancelSearch()}
+        onClick={() => onCloseInput()}
       />
     </form>
   );
