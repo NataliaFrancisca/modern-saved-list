e818786551908a54dd13f1149776de618478ace6
Natalia Francisca <nathifrancisca@gmail.com>
2023-10-04 23:08:07 -0300

    feat: remove the nookies and get the current user using the auth

--- src/app/firebase/database/resource.ts
+++ src/app/firebase/database/resource.ts
@@ -1,14 +1,20 @@
-import { db } from "../config";
+import { auth, db } from "../config";
 import { getDoc, doc } from "firebase/firestore";
 import { getUserCookie } from "@/app/utils/local-storage/save-user";
 
 export const getResource = async() => {
-    const user = getUserCookie();
-    const docRef = doc(db, 'users', user.uid);
+    const user = auth.currentUser;
 
-    return await getDoc(docRef).then((result) => {
-        return result.data();
-    }).catch((error) => {
-        return error;
-    })
+    if(user){
+        const docRef = doc(db, 'users', user.uid);
+
+        return await getDoc(docRef).then((result) => {
+            return result.data();
+        }).catch((error) => {
+            return error;
+        })
+    }
+
+    return "user not connect"
+   
 };
