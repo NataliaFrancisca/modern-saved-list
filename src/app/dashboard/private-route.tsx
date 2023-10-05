9ce03e9c52af2efce551ebb4c588bb9e1caa2d9a
Natalia Francisca <nathifrancisca@gmail.com>
2023-10-04 23:08:37 -0300

    feat: use the onAuthStateChange firebase function to get the current user

--- src/app/dashboard/private-route.tsx
+++ src/app/dashboard/private-route.tsx
@@ -2,6 +2,9 @@ import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { getUserCookie } from '../utils/local-storage/save-user';
 import Loader from '../components/loader';
+import { useGetCurrentUser } from '../hooks/useGetCurrentUser';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../firebase/config';
 
 interface PrivateRouteProps {
   children: React.ReactNode;
@@ -10,18 +13,16 @@ interface PrivateRouteProps {
 
 function PrivateRoute({ children, childrenClass }: PrivateRouteProps) {
   const router = useRouter();
-  const userInformation = getUserCookie();
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    if (!userInformation) {
-      router.push('/');
-      return;
-    }
-
-    if (userInformation) {
-      setLoading(!loading);
-    }
+    onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        setLoading(!loading);
+      } else {
+        router.push('/');
+      }
+    });
   }, []);
 
   return loading ? (
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import AppTitle from './components/app-title';
 import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from './firebase/config';
 import { useEffect } from 'react';
-import { getUserCookie } from './utils/local-storage/save-user';
 import { User } from './types/types';
+import { useGetCurrentUser } from './hooks/useGetCurrentUser';
 
 export default function Home() {
   const router = useRouter();
@@ -15,11 +15,14 @@ export default function Home() {
   };
 
   useEffect(() => {
-    const userCookie = getUserCookie();
-    console.log("I'M beign called at page");
-    if (userCookie) {
-      router.push('/dashboard');
-    }
+    onAuthStateChanged(auth, (currentUser) => {
+      console.log(currentUser);
+      if (currentUser) {
+        router.push('/dashboard');
+      } else {
+        router.push('/');
+      }
+    });
   }, []);
 
   return (
