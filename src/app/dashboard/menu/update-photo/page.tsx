7f40a7d4e4f7fbec25b1a25755c41e3b49ce4b19
Natalia Francisca <nathifrancisca@gmail.com>
2023-10-05 22:22:15 -0300

    feat: add loader at the component; add a default checked to show the user image before it change

--- src/app/dashboard/menu/update-photo/page.tsx
+++ src/app/dashboard/menu/update-photo/page.tsx
@@ -1,7 +1,9 @@
 'use client';
 import AppTitle from '@/app/components/app-title';
 import ButtonDefault from '@/app/components/button-default';
+import Loader from '@/app/components/loader';
 import { useUpdateUserPhoto } from '@/app/hooks/useUpdateUser';
+import { getUserCookie } from '@/app/utils/local-storage/save-user';
 import { user_photos_options } from '@/app/utils/user-profile-photo/photos';
 import { useState } from 'react';
 
@@ -11,39 +13,44 @@ type Photo = {
 };
 
 const UpdatePhoto = () => {
-  const [updatedPhoto, setUpdatedPhoto] = useState<Photo>(
-    user_photos_options[0]
+  const currentPhoto = getUserCookie();
+  const [updatedPhoto, setUpdatedPhoto] = useState<Photo>(currentPhoto.photo);
+  const { loading, submitUpdateUserPhoto } = useUpdateUserPhoto(
+    updatedPhoto.src
   );
-  const { error, submitUpdateUserPhoto } = useUpdateUserPhoto(updatedPhoto.src);
 
-  console.log(updatedPhoto);
   return (
     <section className="__grid-illustrations">
       <AppTitle />
 
-      <form className="form-element" onSubmit={() => submitUpdateUserPhoto()}>
-        <legend className="page-legend">UPDATE PHOTO.</legend>
-        <div className="grid">
-          {user_photos_options.map((photo: Photo) => (
-            <div>
-              <input
-                type="radio"
-                id={`input-selected-image-id${photo.id}`}
-                name="selected-image"
-                onChange={() => setUpdatedPhoto(photo)}
-              />
-              <label
-                htmlFor={`input-selected-image-id${photo.id}`}
-                key={photo.id}
-              >
-                <img src={photo.src} />
-              </label>
-            </div>
-          ))}
-        </div>
+      {loading ? (
+        <Loader color={'GREEN'} />
+      ) : (
+        <form className="form-element" onSubmit={() => submitUpdateUserPhoto()}>
+          <legend className="page-legend">UPDATE PHOTO.</legend>
+          <div className="grid">
+            {user_photos_options.map((photo: Photo, key) => (
+              <div key={key}>
+                <input
+                  type="radio"
+                  id={`input-selected-image-id${photo.id}`}
+                  name="selected-image"
+                  defaultChecked={currentPhoto.photo == photo.src}
+                  onChange={() => setUpdatedPhoto(photo)}
+                />
+                <label
+                  htmlFor={`input-selected-image-id${photo.id}`}
+                  key={photo.id}
+                >
+                  <img src={photo.src} />
+                </label>
+              </div>
+            ))}
+          </div>
 
-        <ButtonDefault btnName="UPDATE" />
-      </form>
+          <ButtonDefault btnName="UPDATE" />
+        </form>
+      )}
     </section>
   );
 };
