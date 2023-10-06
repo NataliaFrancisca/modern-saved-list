f2355cf80716dc9b7a1eb37263650b6dc4dc959b
Natalia Francisca <nathifrancisca@gmail.com>
2023-10-06 20:38:05 -0300

    feat: remove imports

--- src/app/firebase/database/resource.ts
+++ src/app/firebase/database/resource.ts
@@ -1,20 +1,19 @@
 import { auth, db } from "../config";
 import { getDoc, doc } from "firebase/firestore";
-import { getUserCookie } from "@/app/utils/local-storage/save-user";
 
 export const getResource = async() => {
-    const user = auth.currentUser;
+  const user = auth.currentUser;
 
-    if(user){
-        const docRef = doc(db, 'users', user.uid);
+  if(user){
+    const docRef = doc(db, 'users', user.uid);
 
-        return await getDoc(docRef).then((result) => {
-            return result.data();
-        }).catch((error) => {
-            return error;
-        })
-    }
+    return await getDoc(docRef).then((result) => {
+      return result.data();
+    }).catch((error) => {
+      return error;
+    })
+  }
 
-    return "user not connect"
+  return "user not connect"
    
 };
