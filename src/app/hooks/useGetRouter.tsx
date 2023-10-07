aa18fc90b06e58104cf6ec158a4760c38cd15a7d
Natalia Francisca <nathifrancisca@gmail.com>
2023-10-06 21:47:22 -0300

    feat: delete the files to create the new ones

--- src/app/hooks/useGetRouter.tsx
+++
@@ -1,7 +0,0 @@
-import { useSearchParams } from 'next/navigation';
-
-export const useGetRouter = () => {
-  const routerSearch = useSearchParams();
-  const filterRouter = routerSearch.get('filter') || 'allcontent';
-  return filterRouter.toUpperCase();
-};
