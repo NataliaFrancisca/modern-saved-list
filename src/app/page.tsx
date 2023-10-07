65dc209f004907f8790d4c428d6f0765f532ed8b
Natalia Francisca <nathifrancisca@gmail.com>
2023-10-06 21:45:34 -0300

    refactor: refactor the home page with new styles tags and components;

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,18 @@
 'use client';
 import { useRouter } from 'next/navigation';
-import AppTitle from './components/app-title';
 import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from './firebase/config';
 import { useEffect } from 'react';
 
+import AppName from './component/app-name';
+import DefaultButton from './component/default-button';
+
 export default function Home() {
   const router = useRouter();
 
-  const onNav = () => {
+  function onNavigation() {
     router.push('/register');
-  };
+  }
 
   useEffect(() => {
     onAuthStateChanged(auth, (currentUser) => {
@@ -23,16 +25,15 @@ export default function Home() {
   }, [router]);
 
   return (
-    <main className="page">
-      <AppTitle />
+    <main className="home-component">
+      <AppName />
+
       <img
-        className="page-image"
-        src="illustration/illustration-small-size.svg"
+        className="home_illustration"
+        src="./illustration/illustration-small-size.svg"
       />
 
-      <button className="page-button" onClick={onNav}>
-        GET STARTED
-      </button>
+      <DefaultButton message="GET STARTED" buttonEvent={onNavigation} />
     </main>
   );
 }
