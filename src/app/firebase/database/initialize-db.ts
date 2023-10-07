bb475aaab8457f1bf668303340dc173146c6d44b
Natalia Francisca <nathifrancisca@gmail.com>
2023-10-07 20:39:00 -0300

    feat; delete the folder firebase

--- src/app/firebase/database/initialize-db.ts
+++
@@ -1,9 +0,0 @@
-import { doc, setDoc } from 'firebase/firestore';
-import { auth, db } from '../config';
-
-export const initializeDB = async () => {
-  const user = auth.currentUser;
-  await setDoc(doc(db, 'users', `${user?.uid}`), {
-    content: []
-  });
-};
