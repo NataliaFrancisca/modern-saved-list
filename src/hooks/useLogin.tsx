16cd20a84b0da6e2b95f7c8fbc29552091e5c767
Natalia Francisca <nathifrancisca@gmail.com>
2023-10-11 22:06:34 -0300

    feat: create the custom hook to handle with input validation and firebase connection

---
+++ src/hooks/useLogin.tsx
@@ -0,0 +1,54 @@
+import { signIn, signInWithGoogle } from '@/firebase/auth/signin';
+import { TErrorLoginForm, TLoginForm } from '@/ts/types';
+import { validateFormLogin } from '@/utils/form-validations/validate-form';
+import { setUserCookie } from '@/utils/nookies/save-user';
+import { useState } from 'react';
+
+export const UseLogin = (loginForm: TLoginForm) => {
+  const [error, setError] = useState<TErrorLoginForm>();
+
+  const onSubmit = async () => {
+    event?.preventDefault();
+    const validateResult = validateFormLogin(loginForm);
+    setError(validateResult.objError);
+
+    if (!validateResult.hasError) {
+      const firebaseLogin = await signIn(loginForm.email, loginForm.password);
+
+      if (firebaseLogin.data) {
+        setUserCookie(firebaseLogin.data.user);
+        return;
+      }
+
+      if (firebaseLogin.message.includes('Error')) {
+        const validateResult = validateFormLogin(
+          loginForm,
+          firebaseLogin.message
+        );
+        setError(validateResult.objError);
+        return;
+      }
+    }
+  };
+
+  const onSubmitWithGoogle = async () => {
+    event?.preventDefault();
+    const firebaseLogin = await signInWithGoogle();
+
+    if (firebaseLogin.data) {
+      setUserCookie(firebaseLogin.data.user);
+      return;
+    }
+
+    if (firebaseLogin.message.includes('Error')) {
+      setError({
+        email: false,
+        password: false,
+        google: 'Something wrong with Google Login, try again'
+      });
+      return;
+    }
+  };
+
+  return { error, onSubmit, onSubmitWithGoogle };
+};
