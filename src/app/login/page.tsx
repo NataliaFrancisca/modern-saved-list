f4de771ee146f979922ca58986d27bcd996f41df
Natalia Francisca <nathifrancisca@gmail.com>
2023-10-11 22:05:13 -0300

    feat: use the custom hook to submit the form and handle with errors

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,29 +1,58 @@
+'use client';
+import { useState } from 'react';
+import { UseLogin } from '@/hooks/useLogin';
 import AppName from '../component/app-name';
 import DefaultButton from '../component/default-button';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const { error, onSubmit, onSubmitWithGoogle } = UseLogin({ email, password });
+
   return (
     <main className="default-page">
       <AppName />
 
-      <form className="form-component">
+      <form className="form-component" onClick={() => onSubmit()}>
         <legend>LOGIN.</legend>
 
+        {error?.google && (
+          <span className="error-login_google">
+            PROBLEM DOING LOGIN WITH GOOGLE, <br /> TRY AGAIN
+          </span>
+        )}
+
         <fieldset className="form-group">
-          <label>Email:</label>
-          <input type="email" required />
+          <label htmlFor="email-input">Email:</label>
+          <input
+            type="email"
+            required
+            id="email-input"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          {error && <span>{error.email}</span>}
         </fieldset>
 
         <fieldset className="form-group">
-          <label>Password:</label>
-          <input type="password" required />
+          <label htmlFor="password-input">Password:</label>
+          <input
+            type="password"
+            required
+            id="password-input"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          {error && <span>{error.password}</span>}
         </fieldset>
 
         <DefaultButton message="LOGIN" />
 
         <span className="txt-or">OR</span>
 
-        <button className="defaultButton-component _google">
+        <button
+          className="defaultButton-component _google"
+          onClick={() => onSubmitWithGoogle()}
+        >
           LOGIN WITH GOOGLE
         </button>
 
