0236aa498bac3d1173b5b4640043b55302003a43
Natalia Francisca <nathifrancisca@gmail.com>
2023-12-25 19:43:01 -0300

    feat: add an id

--- src/app/component/loader.tsx
+++ src/app/component/loader.tsx
@@ -1,5 +1,5 @@
 const Loader = (props: { color: string }) => {
-  return <span className={`loader loader_${props.color}`}></span>;
+  return <span className={`loader loader_${props.color}`} id="loader"></span>;
 };
 
 export default Loader;
