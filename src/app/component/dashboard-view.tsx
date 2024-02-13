56b9d2ac3e3b1193fa8aea9a0ee61b1a8b767fba
NataliaFrancisca <nathifrancisca@gmail.com>
2024-02-13 20:45:09 -0300

    feat: use the Content component and set the router function to the button

--- src/app/component/dashboard-view.tsx
+++ src/app/component/dashboard-view.tsx
@@ -3,9 +3,13 @@
 import { useGetRouter } from "@/hooks/useGetRouter";
 import Loader from "./loader";
 import { UseFetchContent } from "@/hooks/useFetchContent";
+import { useRouter } from "next/navigation";
+import Content from "./content";
 
 const DashboardView = (props: {searchResult: string}) => {
   const filter = useGetRouter();
+  const router = useRouter();
+
   const { loading, data } = UseFetchContent(props.searchResult, filter);
 
   if(loading){
@@ -14,10 +18,16 @@ const DashboardView = (props: {searchResult: string}) => {
 
   return(
     <section className="__dashboard-view">
-      {data?.length == 0 && <p>NOTHING HERE :(</p>}
-      {data?.map((element, key) => (
-        <p key={key}>{element.title} | {element.type}</p>
+      {data?.length == 0 && <p>NOTHING HERE :( </p>}
+
+      <button className="__button-add" onClick={() => router.push('/dashboard/save-content')}>
+        <img src="icon/add.svg" alt="add icon" />
+      </button>
+    
+      {data && data.map((content, key) => (
+        <Content data={content} key={key}/>
       ))}
+
     </section>
   )
 }
